6a84c260fe827dd8f9ac02a7fbf75ebc83e9aa43
jhonalidoui-al <jhon.alido.ui@phinmaed.com>
2026-03-01 12:49:00 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,7 @@
-const cacheName = 'todo-v3';
-const assets = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', (e) => {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(assets))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.open("todo-list-cache").then(function(cache) {
+      return cache.addAll(["index.html"]);
+    })
   );
 });
